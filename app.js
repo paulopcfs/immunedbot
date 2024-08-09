d07a28d890831940b6e00adfc1656e084e8b8cb8
paulopcfs <140545686+paulopcfs@users.noreply.github.com>
2024-08-09 11:33:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+require('dotenv').config();
+const fs = require('fs');
+const csv = require('csv-parser');
+const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 const sql = require('mssql');
-require('dotenv').config();
+const rateLimit = require('express-rate-limit').default;
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const apiKey = process.env.Z_API_KEY;
-const apiUrl = process.env.Z_API_URL;
+// Configuração de proxy
+app.set('trust proxy', 1);
+
+const instanceId = process.env.ZAPI_INSTANCE_ID;
+const instanceToken = process.env.ZAPI_INSTANCE_TOKEN;
+const clientToken = process.env.CLIENT_TOKEN;
+const apiUrl = `https://api.z-api.io/instances/${instanceId}/token/${instanceToken}/send-text`;
+
+const contactsFilePath = 'contacts.csv';
 
 const dbConfig = {
     user: process.env.DB_USER,
@@ -19,12 +29,29 @@ const dbConfig = {
     options: {
         encrypt: true,
         enableArithAbort: true
-    }
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    connectionTimeout: 30000,
+    requestTimeout: 30000
 };
+
+let pool;
+const poolConnect = sql.connect(dbConfig)
+    .then(p => {
+        pool = p;
+        console.log('Conectado ao banco de dados');
+    })
+    .catch(err => {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    });
 
 const questions = [
     { question: "Nos últimos 7 dias, com que frequência você sentiu dor nas articulações?", options: ["1. Nunca", "2. Ocasionalmente", "3. Frequentemente", "4. Diariamente"] },
-    { question: "Nos últimos 7 dias, com que intensidade você classificaria sua dor nas articulações em uma escala de 0 a 10?", options: ["1. 0", "2. 1-3", "3. 4-6", "4. 7-10"] },
+    { question: "Nos últimos 7 dias, com que intensidade você classificaria sua dor nas articulações em uma escala de 0 a 10, considerando 0 sem dor e 10, muita dor?", options: ["1. 0", "2. 1-3", "3. 4-6", "4. 7-10"] },
     { question: "Você teve dificuldade em realizar suas atividades diárias devido à rigidez nas articulações?", options: ["1. Nenhuma", "2. Leve", "3. Moderada", "4. Grave"] },
     { question: "Você se sentiu cansado ou exausto sem motivo aparente nos últimos 7 dias?", options: ["1. Nunca", "2. Ocasionalmente", "3. Frequentemente", "4. Diariamente"] },
     { question: "Você notou algum inchaço em suas articulações nos últimos 7 dias?", options: ["1. Nenhum", "2. Leve", "3. Moderado", "4. Grave"] },
@@ -34,112 +61,185 @@ const questions = [
 ];
 
 let responses = {};
-let introMessageSent = false;
 
-async function sendQuestion(phoneNumber, question) {
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minuto
+    max: 60, // Limita cada IP a 60 requisições por janela de tempo
+    message: "Muitas requisições criadas a partir deste IP, por favor tente novamente após um minuto"
+});
+
+app.use(limiter);
+
+function sendMessage(phoneNumber, message) {
+    const url = apiUrl;
+    const data = {
+        phone: phoneNumber,
+        message: message
+    };
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Client-Token': clientToken
+    };
+
+    return axios.post(url, data, { headers })
+        .then(response => {
+            console.log(`Mensagem enviada para ${phoneNumber}: ${message}`);
+        })
+        .catch(error => {
+            console.error(`Erro ao enviar mensagem para ${phoneNumber}:`, error.response ? error.response.data : error.message);
+        });
+}
+
+async function sendQuestion(phoneNumber, questionIndex) {
+    const question = questions[questionIndex];
     const optionsText = question.options.join('\n');
+    const message = `${question.question}\nPor favor, responda com o número correspondente:\n${optionsText}`;
+
+    console.log(`Enviando pergunta ${questionIndex + 1} para ${phoneNumber}`);
 
     try {
-        await axios.post(`${apiUrl}/send-message`, {
-            phone: phoneNumber,
-            message: `${question.question}\nPor favor, responda com o número correspondente:\n${optionsText}`
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'apiKey': apiKey
-            }
-        });
+        await sendMessage(phoneNumber, message);
     } catch (err) {
-        console.error(`Erro ao enviar mensagem para ${phoneNumber}: ${err.response ? err.response.data : err.message}`);
+        console.error(`Erro ao enviar pergunta ${questionIndex + 1} para ${phoneNumber}: ${err.message}`);
     }
 }
 
-app.post('/webhook', async (req, res) => {
-    const incomingMsg = req.body.Body.trim();
-    const phoneNumber = req.body.From.replace('whatsapp:', '');
+async function handleCompletion(phoneNumber, userResponses) {
+    try {
+        let score = 0;
+        userResponses.answers.forEach((answer, index) => {
+            const selectedAnswerIndex = questions[index].options.indexOf(answer) + 1;
+            score += selectedAnswerIndex;
+        });
 
-    if (!introMessageSent) {
-        try {
-            await axios.post(`${apiUrl}/send-message`, {
-                phone: phoneNumber,
-                message: "Olá! Este é um questionário sobre saúde reumatológica realizado pela Immuned. Por favor, responda às perguntas a seguir para que possamos avaliar sua condição."
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'apiKey': apiKey
-                }
-            });
-            introMessageSent = true;
-            responses[phoneNumber] = { currentQuestion: 0, answers: [] };
-            sendQuestion(phoneNumber, questions[0]);
-        } catch (err) {
-            console.error(`Erro ao enviar mensagem para ${phoneNumber}: ${err.response ? err.response.data : err.message}`);
+        let condition = "Leve";
+        if (score >= 8 && score <= 14) condition = "Moderada";
+        else if (score >= 15 && score <= 21) condition = "Grave";
+        else if (score >= 22) condition = "Muito Grave";
+
+        let finalMessage = "Obrigado por responder às perguntas!";
+        if (condition === "Muito Grave") {
+            finalMessage += " Recomendamos que você consulte um médico imediatamente devido à gravidade da sua condição.";
         }
+
+        await sendMessage(phoneNumber, finalMessage);
+        await saveResponsesToDatabase(phoneNumber, userResponses.answers);
+
+        delete responses[phoneNumber];
+    } catch (error) {
+        console.error(`Erro ao processar a conclusão do questionário para ${phoneNumber}:`, error);
+    }
+}
+
+async function saveResponsesToDatabase(phoneNumber, answers) {
+    try {
+        const poolRequest = pool.request();
+
+        // Adicione todos os parâmetros necessários com nomes únicos
+        poolRequest.input('phone', sql.VarChar, phoneNumber);
+        poolRequest.input('answer1', sql.VarChar, answers[0] || null);
+        poolRequest.input('answer2', sql.VarChar, answers[1] || null);
+        poolRequest.input('answer3', sql.VarChar, answers[2] || null);
+        poolRequest.input('answer4', sql.VarChar, answers[3] || null);
+        poolRequest.input('answer5', sql.VarChar, answers[4] || null);
+        poolRequest.input('answer6', sql.VarChar, answers[5] || null);
+        poolRequest.input('answer7', sql.VarChar, answers[6] || null);
+        poolRequest.input('answer8', sql.VarChar, answers[7] || null);
+
+        // Execute a consulta com os parâmetros nomeados
+        await poolRequest.query(`
+            INSERT INTO Responses (Phone, Answer1, Answer2, Answer3, Answer4, Answer5, Answer6, Answer7, Answer8)
+            VALUES (@phone, @answer1, @answer2, @answer3, @answer4, @answer5, @answer6, @answer7, @answer8)
+        `);
+
+        console.log('Respostas salvas com sucesso');
+    } catch (error) {
+        console.error('Erro ao inserir no banco de dados:', error);
+    }
+}
+
+fs.createReadStream(contactsFilePath)
+    .pipe(csv())
+    .on('headers', (headers) => {
+        console.log('Cabeçalhos processados:', headers);
+    })
+    .on('data', (row) => {
+        const phoneNumber = row['numero'] ? row['numero'].replace('+', '') : '';
+        if (phoneNumber) {
+            sendMessage(phoneNumber, 'Olá! Este é um questionário sobre saúde reumatológica realizado pela Immuned. Por favor, responda às perguntas a seguir para que possamos avaliar sua condição.')
+                .then(() => {
+                    responses[phoneNumber] = { currentQuestion: 0, answers: [] };
+                    sendQuestion(phoneNumber, 0);
+                });
+        } else {
+            console.log('Linha vazia ou número de telefone ausente');
+        }
+    })
+    .on('end', () => {
+        console.log('Arquivo CSV processado com sucesso');
+    });
+
+app.post('/webhook', async (req, res) => {
+    console.log('Requisição recebida no webhook:', req.body);
+
+    const { phone, type, text } = req.body;
+
+    // Verifica se o tipo de mensagem é suportado e se contém um texto válido
+    if (type !== 'Message' || !text || (typeof text !== 'string' && (!text.body || typeof text.body !== 'string'))) {
+        console.log(`Tipo de mensagem não suportado ou texto vazio: Type: ${type}, Text: ${JSON.stringify(text)}`);
+        return res.sendStatus(200);
+    }
+
+    // Se o texto for um objeto com a propriedade message, utilize-a
+    const body = typeof text === 'string' ? text : text.message;
+
+    if (!body || !phone) {
+        console.log(`Parâmetros do corpo da mensagem inválidos: Body: ${body}, From: ${phone}`);
+        return res.sendStatus(400);
+    }
+
+    console.log(`Recebendo mensagem de ${phone}: ${body}`);
+
+    const phoneNumber = phone.replace('-', '');
+    if (!responses[phoneNumber]) {
+        console.log(`Iniciando nova conversa para o número ${phoneNumber}`);
+        responses[phoneNumber] = {
+            currentQuestion: 0,
+            answers: []
+        };
+        await sendQuestion(phoneNumber, 0);  // Envia a primeira pergunta
         return res.sendStatus(200);
     }
 
     const userResponses = responses[phoneNumber];
+    console.log(`Resposta atual do usuário ${phoneNumber}:`, userResponses);
+
     if (userResponses.currentQuestion < questions.length) {
         const question = questions[userResponses.currentQuestion];
-        const selectedOption = parseInt(incomingMsg, 10);
+        const selectedOption = parseInt(body);
 
-        if (selectedOption > 0 && selectedOption <= question.options.length) {
+        if (selectedOption && selectedOption >= 1 && selectedOption <= question.options.length) {
             userResponses.answers.push(question.options[selectedOption - 1]);
             userResponses.currentQuestion++;
 
             if (userResponses.currentQuestion < questions.length) {
-                sendQuestion(phoneNumber, questions[userResponses.currentQuestion]);
+                await sendQuestion(phoneNumber, userResponses.currentQuestion);
             } else {
-                // Lógica de cálculo do score e mensagem final
-                let score = 0;
-                userResponses.answers.forEach((answer, index) => {
-                    score += answer.startsWith(index + 1) ? index : 0;
-                });
-
-                let condition = "Leve";
-                if (score >= 8 && score <= 14) condition = "Moderada";
-                else if (score >= 15 && score <= 21) condition = "Grave";
-                else if (score >= 22) condition = "Muito Grave";
-
-                let finalMessage = "Obrigado por responder às perguntas!";
-                if (condition === "Muito Grave") {
-                    finalMessage += " Recomendamos que você consulte um médico imediatamente devido à gravidade da sua condição.";
-                }
-
-                try {
-                    await axios.post(`${apiUrl}/send-message`, {
-                        phone: phoneNumber,
-                        message: finalMessage
-                    }, {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'apiKey': apiKey
-                        }
-                    });
-                } catch (err) {
-                    console.error(`Erro ao enviar mensagem para ${phoneNumber}: ${err.response ? err.response.data : err.message}`);
-                }
-
-                // Armazenar as respostas no banco de dados
-                try {
-                    await sql.connect(dbConfig);
-                    for (const answer of userResponses.answers) {
-                        await sql.query`INSERT INTO Responses (phoneNumber, question, response) VALUES (${phoneNumber}, ${questions[responses[phoneNumber].currentQuestion].question}, ${answer})`;
-                    }
-                } catch (err) {
-                    console.error('Erro ao conectar ou inserir no banco de dados:', err);
-                }
-
-                // Reiniciar o questionário
-                delete responses[phoneNumber];
-                introMessageSent = false;
+                await handleCompletion(phoneNumber, userResponses);
             }
         } else {
-            sendQuestion(phoneNumber, question);
+            await sendMessage(phoneNumber, `Resposta inválida. Por favor, responda com um número de 1 a ${question.options.length}`);
         }
+    } else {
+        console.log(`Todas as perguntas já foram respondidas para o número ${phoneNumber}`);
     }
 
     res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
